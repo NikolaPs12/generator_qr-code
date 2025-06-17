d415823c895b8136154c393ae85be1855c1b76ff
nikola <psenickijkola@email.com>
2025-06-17 21:37:05 +0300

    изменен __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask
 from .routes.main import main
 import os
 
@@ -6,16 +6,20 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = '123456767890'
-    app.config['UPLOAD_FOLDER'] = 'pdf-files'
-    app.config['QR_CODE_FOLDER'] = os.path.join('static', 'qr-codes')  # Сохраняем в static/qr-codes
-    app.config['ALLOWED_EXTENSIONS'] = {'pdf'}
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB максимальный размер файла
 
-    # Создаем необходимые папки
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    os.makedirs(app.config['QR_CODE_FOLDER'], exist_ok=True)
+    # Безопасная папка загрузки
+    upload_folder = '/tmp/uploads/pdfs' if not app.debug else 'pdf-files'
+    qr_folder = os.path.join(app.root_path, 'static', 'qr-codes')
+
+    app.config['UPLOAD_FOLDER'] = upload_folder
+    app.config['QR_CODE_FOLDER'] = qr_folder
+    app.config['ALLOWED_EXTENSIONS'] = {'pdf'}
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
+
+    # Создаём папки (даже в /tmp)
+    os.makedirs(upload_folder, exist_ok=True)
+    os.makedirs(qr_folder, exist_ok=True)
 
     app.register_blueprint(main)
 
     return app
-    
